6f20c736bfd3c0ae91327d6bb03fdc13622c48f6
spm086 <sonia.perez.smr@gmail.com>
2024-03-27 14:51:13 +0100

    He creado los atributos, el constructor y get y set.

--- src/escuela/Alumno.java
+++ src/escuela/Alumno.java
@@ -2,4 +2,39 @@ package escuela;
 
 public class Alumno {
 
+	private String nombre;
+	private int edad;
+	private String curso;
+
+	Alumno(String nombre, int edad, String curso) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.curso = curso;
+
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
 }
--- src/escuela/Profesor.java
+++ src/escuela/Profesor.java
@@ -2,4 +2,38 @@ package escuela;
 
 public class Profesor {
 
+	private int id;
+	private String nombre;
+	private String especialidad;
+
+	Profesor(int id, String nombre, String especialidad) {
+		this.id = id;
+		this.nombre = nombre;
+		this.especialidad = especialidad;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEspecialidad() {
+		return especialidad;
+	}
+
+	public void setEspecialidad(String especialidad) {
+		this.especialidad = especialidad;
+	}
+
 }
