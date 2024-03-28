c0f5b0a90aa5a52c0a6918d2a26b7b5f2debb236
spm086 <sonia.perez.smr@gmail.com>
2024-03-28 23:31:33 +0100

    Aqui lo que he hecho ha sido crear el javadoc y lo he incluido a la carpeta gitignore, he puesto todas las etiquetas junto con un metodo deprecated e implementado uno actualizado.

--- src/escuela/Alumno.java
+++ src/escuela/Alumno.java
@@ -2,11 +2,26 @@ package escuela;
 
 public class Alumno {
 
+	/**
+	 * @author Sonia
+	 * @version 1.0
+	 * @since 27/03/2024
+	 */
+
 	private String nombre;
 	private int edad;
 	private String curso;
 	private int nota;
 	private int suspensos;
+
+	/**
+	 * Estos parametros son necesarios para la creacion del objeto Alumno.
+	 * 
+	 * @param nombre
+	 * @param edad
+	 * @param curso
+	 * @param nota
+	 */
 
 	Alumno(String nombre, int edad, String curso, int nota) {
 		this.nombre = nombre;
@@ -16,59 +31,114 @@ public class Alumno {
 
 	}
 
+	/**
+	 * @return Este metodo lo que hace es devolvernos el nombre.
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * 
+	 * @param nombre Esto es el nombre del alumno y con este metodo set puedo
+	 *               modificarlo
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * 
+	 * @return Este lo que hace es devolvernos la edad
+	 */
 	public int getEdad() {
 		return edad;
 	}
 
+	/**
+	 * 
+	 * @param edad Esto es la edad del alumno y con este metodo set podemos
+	 *             modificarlo
+	 */
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
 
+	/**
+	 * 
+	 * @return Esto lo que hace es devolver el curso del Alumno
+	 */
 	public String getCurso() {
 		return curso;
 	}
 
+	/**
+	 * 
+	 * @param curso Esto es el curso en el que esta el alumno y con este metodo
+	 *              podemos modificarlo
+	 */
 	public void setCurso(String curso) {
 		this.curso = curso;
 	}
+
+	/**
+	 * 
+	 * @return Aqui lo que hace es devolver la nota de alumno
+	 */
 
 	public int getNota() {
 		return nota;
 	}
 
+	/**
+	 * 
+	 * @param nota Esta es la nota del alumno y con este metodo podemos modificarla
+	 */
 	public void setNota(int nota) {
 		this.nota = nota;
 	}
 
+	/**
+	 * 
+	 * @return Aqui lo que hace es devolver los suspensos
+	 */
 	public int getSuspensos() {
 		return suspensos;
 	}
 
+	/**
+	 * 
+	 * @param suspensos Con este metodo tambien podemos modificarlo en caso de que
+	 *                  queramos cambiar algo
+	 */
 	public void setSuspensos(int suspensos) {
 		this.suspensos = suspensos;
 	}
 
+	/**
+	 * Este metodo lo que hace es darnos todos los datos del alumno.
+	 */
 	@Override
 	public String toString() {
 		return "Alumno [nombre=" + nombre + ", edad=" + edad + ", curso=" + curso + ", nota=" + nota + ", suspensos="
 				+ suspensos + "]";
 	}
 
+	/**
+	 * Este metodo muestra por pantalla un mensaje de presentacion del alumno.
+	 */
 	public void presentarse() {
 		System.out.println("Hola, soy " + nombre + ", tengo " + edad + " años y curso " + curso);
 	}
 
+	/**
+	 * Este metodo dependiendo de la nota que tiene el alumno imprime un mensaje
+	 * diciendo si debe estudiar o no.
+	 */
+
 	public void estudiar() {
 
-		if (nota < 5) {
+		if (this.nota < 5) {
 
 			System.out.println(nombre + " está estudiando para recuperar");
 		} else {
@@ -76,9 +146,15 @@ public class Alumno {
 		}
 	}
 
+	/**
+	 * Este metodo cuenta los suspensos que hay en total.
+	 * 
+	 * @return Devuelve un int con los numeros de suspensos que hay en total
+	 */
+
 	public int contarSuspensos() {
 		int suspensos = 0;
-		if (nota < 5) {
+		if (this.nota < 5) {
 			suspensos++;
 		}
 		return suspensos;
--- src/escuela/Profesor.java
+++ src/escuela/Profesor.java
@@ -2,49 +2,103 @@ package escuela;
 
 public class Profesor {
 
+	/**
+	 * @author Sonia
+	 * @version 1.0
+	 * @since 27/03/2024
+	 */
+
 	private int id;
 	private String nombre;
 	private String especialidad;
 
+	/**
+	 * Estos parametros son necesarios para la creacion del objeto Profesor.
+	 * 
+	 * @param id
+	 * @param nombre
+	 * @param especialidad
+	 */
 	Profesor(int id, String nombre, String especialidad) {
 		this.id = id;
 		this.nombre = nombre;
 		this.especialidad = especialidad;
 	}
 
+	/**
+	 * 
+	 * @return Aqui devuelve el id.
+	 */
+
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * 
+	 * @param id Es el identificador del profesor con este metodo podemos
+	 *           modificarlo.
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	/**
+	 * 
+	 * @return Aqui devuelve el nombre.
+	 */
 
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * 
+	 * @param nombre Este es el nombre del profesor y con este metodo podemos
+	 *               modificarlo.
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
+	/**
+	 * 
+	 * @return Aqui devuelve la especialidad del profesor.
+	 */
 
 	public String getEspecialidad() {
 		return especialidad;
 	}
 
+	/**
+	 * 
+	 * @param especialidad Es la especialidad del profesor y con este metodo podemos
+	 *                     modificarla.
+	 */
 	public void setEspecialidad(String especialidad) {
 		this.especialidad = especialidad;
 	}
 
+	/**
+	 * Este metodo lo que hace es poder ver los datos del profesor.
+	 */
 	@Override
 	public String toString() {
 		return "Profesor [id=" + id + ", nombre=" + nombre + ", especialidad=" + especialidad + "]";
 	}
 
+	/**
+	 * Este metodo muestra por pantalla un mensaje de presentacion del profesor.
+	 */
+
 	public void presentarse() {
 		System.out.println("Hola, soy el profesor " + nombre + ", mi especialidad es " + especialidad);
 	}
 
+	/**
+	 * @param suspensos Este parametro recibe un int con la nota. El metodo segun la
+	 *                  nota determina si esta suspenso o aprobado.
+	 */
 	public void impartirClase(int suspensos) {
 		if (suspensos >= 10) {
 			System.out.println(
@@ -55,11 +109,48 @@ public class Profesor {
 		}
 	}
 
+	/**
+	 * @param nota El parametro nota recoge un int con el numero de la nota. El
+	 *             metodo recoge el parametro nota y nos dice si es mayor a 5 que
+	 *             esta aprobado y si es menor estara suspenso.
+	 */
+
+	/**
+	 * 
+	 * @deprecated Este metodo ya esta obsoleto y se cambia la version ya que vamos
+	 *             a actualizar el metodo.
+	 * @version 1.2 Este metodo seria el nuevo modificado, con la version mas
+	 *          actualizada.
+	 * @see calificarExamenConcurso
+	 * 
+	 */
 	public void calificarExamen(int nota) {
 		if (nota >= 5) {
 			System.out.println("Muy bien, sigue así.");
 		} else {
 			System.out.println("Tienes que aplicarte más.");
+		}
+	}
+
+	/**
+	 * 
+	 * @param nota Este parametro int guarda la nota del alumno y segun se mayor o
+	 *             igual nos dice que debe hacer el alumno, y se elegira al que
+	 *             tenga una nota superior a un 9 para el concurso municipal.
+	 * 
+	 * 
+	 */
+	public void calificarExamenConcurso(int nota) {
+		if (nota >= 5) {
+			System.out.println("Muy bien, sigue así.");
+		}
+		if (nota >= 9.5) {
+			System.out.println("Eres el elegido, para el concurso municipal de programación.");
+		} else {
+
+		}
+		{
+			System.out.println("Tienes que presentarte a la recuperación.");
 		}
 	}
 
